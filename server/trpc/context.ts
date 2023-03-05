72da308893661e86fc4cea890880cf39b8571645
Abdelilah_OU <gxgamer710@gmail.com>
2023-03-05 17:05:02 +0000

    update prisma context

--- server/trpc/context.ts
+++ server/trpc/context.ts
@@ -1,5 +1,22 @@
+import { PrismaClient } from "@prisma/client";
 import { inferAsyncReturnType } from "@trpc/server";
 
-export const createContext = () => {};
+let prisma: PrismaClient | undefined;
+
+const getSession = () => {
+  return new Promise((resolve) => {
+    resolve("");
+  });
+};
+
+export const createContext = async () => {
+  if (!prisma) prisma = new PrismaClient();
+
+  const session = await getSession();
+  return {
+    session,
+    prisma,
+  };
+};
 
 export type Context = inferAsyncReturnType<typeof createContext>;
