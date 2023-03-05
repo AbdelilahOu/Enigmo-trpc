cf8101d5c1f150eaae1b9836cdc83a4cf069b6e5
Abdelilah_OU <gxgamer710@gmail.com>
2023-03-05 16:19:58 +0000

    first commit

---
+++ server/trpc/trpc.ts
@@ -0,0 +1,11 @@
+import { initTRPC } from "@trpc/server";
+import { Context } from "~/server/trpc/context";
+
+const t = initTRPC.context<Context>().create();
+
+/**
+ * Unprotected procedure
+ **/
+export const publicProcedure = t.procedure;
+export const router = t.router;
+export const middleware = t.middleware;
