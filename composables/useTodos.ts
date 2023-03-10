318d379cc8f768af383324eaf29f14871ca773a4
Abdelilah_OU <gxgamer710@gmail.com>
2023-03-10 07:22:25 +0000

    input validation

--- composables/useTodos.ts
+++ composables/useTodos.ts
@@ -2,8 +2,10 @@ const { $client: trpc } = useNuxtApp();
 
 export default () => {
   const createTodo = (todo: newTodo) => trpc.Todos.create.mutate(todo);
-  const deleteTodo = (id: number) => trpc.Todos.delete.mutate();
-  const updateTodo = (id: number) => trpc.Todos.update.mutate();
+  const deleteTodo = (id: number) => trpc.Todos.delete.mutate(id);
+  const updateTodo = (id: number, todo: updateTodo) => {
+    return trpc.Todos.update.mutate({ id, todo });
+  };
   const queryTodos = () => trpc.Todos.findAll.useQuery();
 
   return {
@@ -14,8 +16,11 @@ export default () => {
   };
 };
 
-interface newTodo {
+interface todo {
+  id: number;
   deadline: string;
   status: string;
   todo: string;
 }
+interface newTodo extends Omit<todo, "id"> {}
+interface updateTodo extends Partial<newTodo> {}
--- server/trpc/routes/todos.ts
+++ server/trpc/routes/todos.ts
@@ -2,6 +2,9 @@ import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 
 export const todosRoute = router({
+  findAll: publicProcedure.query(
+    async ({ ctx }) => await ctx.prisma.todos.findMany({})
+  ),
   create: publicProcedure
     .input(
       z.object({
@@ -18,21 +21,30 @@ export const todosRoute = router({
         todo,
       };
     }),
-  findAll: publicProcedure.query(
-    async ({ ctx }) => await ctx.prisma.todos.findMany({})
-  ),
-  update: publicProcedure.mutation(({ ctx }) => {
-    return ctx.prisma.todos.update({
-      where: {
-        id: 1,
-      },
-      data: {},
-    });
-  }),
-  delete: publicProcedure.mutation(({ ctx }) => {
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        todo: z.object({
+          todo: z.string().optional(),
+          deadline: z.string().optional(),
+          status: z.string().optional(),
+        }),
+      })
+    )
+    .mutation(({ input, ctx }) => {
+      const { id, todo } = input;
+      return ctx.prisma.todos.update({
+        where: {
+          id,
+        },
+        data: todo,
+      });
+    }),
+  delete: publicProcedure.input(z.number()).mutation(({ input: id, ctx }) => {
     return ctx.prisma.todos.delete({
       where: {
-        id: 1,
+        id,
       },
     });
   }),
