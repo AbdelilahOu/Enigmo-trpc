d2ecb9ffea9369acecf2a2a99bcac7d7a9aa7ce3
Abdelilah_OU <gxgamer710@gmail.com>
2023-03-11 12:09:08 +0000

    add components

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import useTodos from "../composables/useTodos";
 const { queryTodos, deleteTodo, updateTodo, createTodo } = useTodos();
+
+const { data, refresh, pending } = await queryTodos();
 </script>
 
 <template>
@@ -8,7 +10,9 @@ const { queryTodos, deleteTodo, updateTodo, createTodo } = useTodos();
     <div class="w-full h-full px-1 py-1 md:w-3/4 md:h-5/6 lg:w-1/2">
       <UiInput @on-input="" />
 
-      <div class="w-full h-full flex flex-col"></div>
+      <div class="w-full h-full flex flex-col">
+        <Todo v-for="todo in data" />
+      </div>
     </div>
   </div>
 </template>
