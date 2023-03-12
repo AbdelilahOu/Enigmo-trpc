82d5f1e55fbc0941f8c162635c7bcd4baac26bce
Abdelilah_OU <gxgamer710@gmail.com>
2023-03-12 15:38:49 +0000

    work on ui

---
+++ components/UiButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineEmits(["clicked"]);
+</script>
+
+<template>
+  <button
+    @click="$emit('clicked')"
+    class="w-full h-full rounded-md transition-all duration-200 hover:bg-sky-400/60 border-2 border-sky-400 bg-sky-400 text-white font-semibold"
+  >
+    <slot></slot>
+  </button>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,28 @@
 import useTodos from "../composables/useTodos";
 const { queryTodos, deleteTodo, updateTodo, createTodo } = useTodos();
 const { data, refresh, pending } = await queryTodos();
+
+const newTodo = reactive({
+  todo: "",
+  deadline: "",
+  status: "",
+});
 </script>
 
 <template>
   <div class="w-full h-full flex items-center justify-center">
     <div class="w-full h-full px-1 py-1 md:w-3/5 md:h-5/6 lg:w-1/3">
-      <UiInput @on-input="" />
-
+      <div class="grid grid-cols-2 gap-x-1 grid-rows-[40px_40px_40px]">
+        <div class="col-span-2">
+          <UiInput @on-input="(text) => (newTodo.todo = text)" />
+        </div>
+        <UiInput @on-input="(text) => (newTodo.deadline = text)" />
+        <UiInput @on-input="(text) => (newTodo.status = text)" />
+        <div class="col-span-2">
+          <UiButton @clicked="createTodo(newTodo)"> Add </UiButton>
+        </div>
+      </div>
+      {{ newTodo }}
       <div v-if="pending" class="w-full h-full flex flex-col mt-2">
         <Todo
           @delete:todo="
